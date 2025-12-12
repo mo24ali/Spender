95c573a934dbf39d0ae4f8616ec05845dc1ee7f9
mo24ali <alichere1234@gmail.com>
2025-12-12 22:38:45 +0100

    comm

---
+++ models/card.php
@@ -0,0 +1,19 @@
+<?php
+
+
+    class Card{
+
+        private $conn;
+
+        public function __construct($conn){
+            $this->conn = $conn;
+        }
+        public function addCard(){
+
+        }
+        public function removeCard(){
+
+        }
+    }
+
+?>
